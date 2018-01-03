c41a1daacf085b4baf1d4d4797c0b3969f03b546
jhunter5811 <jhunter@learningmachine.com>
2018-01-03 13:53:01 -0600

    Build?

--- src/index.js
+++ src/index.js
@@ -4,10 +4,4 @@ import ReactDOM from "react-dom";
 
 import App from "./components/App";
 
-// Require Sass file so webpack can build it
-import style from "./styles/style.css";
-if (process.env.NODE_ENV !== "production") {
-  console.log("Looks like we are in development mode!");
-}
-
 ReactDOM.render(<App />, document.getElementById("root"));
--- webpack.prod.js
+++ webpack.prod.js
@@ -5,6 +5,21 @@ const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
   devtool: "source-map",
+  module: {
+    rules: [
+      { test: /\.json$/, use: 'json-loader' },
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
+        loader: 'file-loader!url-loader',
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+    ],
+    loaders: [
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
+    ]
+  },
   plugins: [
     new UglifyJSPlugin({
       sourceMap: true
