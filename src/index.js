8f51cb33a89acbf8ac3bdfc6e070a64448dc74fa
jhunter5811 <jhunter@learningmachine.com>
2018-01-03 15:08:12 -0600

    I dunno

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
-import App from "./components/App";
-import style from "./styles/style.css";
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Require Sass file so webpack can build it
+import style from './styles/style.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
