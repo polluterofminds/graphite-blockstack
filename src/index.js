024111d27a3dd2ceb7f30857781fbbe6a492f5dc
jhunter5811 <jhunter@learningmachine.com>
2018-01-03 13:56:08 -0600

    Package.json fixes

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 import ReactDOM from "react-dom";
 
 import App from "./components/App";
+import style from "./styles/style.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
