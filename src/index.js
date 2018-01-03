aaf25389e6e6deee2fb24b211877f0d75f8bddb2
jhunter5811 <jhunter@learningmachine.com>
2018-01-03 13:33:28 -0600

    All kinds of webpack changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
 
-import App from './components/App';
+import App from "./components/App";
 
 // Require Sass file so webpack can build it
-import style from './styles/style.css';
+import style from "./styles/style.css";
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
+}
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
